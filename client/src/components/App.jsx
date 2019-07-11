bfda5976bc8b461c2dd1a426efeb8279745b6e4b
Robert Battaglia <r083rt164@gmail.com>
2019-07-11 14:48:01 -0400

    get data from api and save to app state

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,27 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import Chart from './Chart';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { data: null };
+    this.getData = this.getData.bind(this);
+  }
+
+  getData() {
+    axios
+      .get(`https://api.coindesk.com/v1/bpi/historical/close.json`)
+      .then(({ data }) => {
+        data = data.bpi;
+        this.setState({ data });
+      });
+  }
+
+  componentDidMount() {
+    this.getData();
+  }
+
   render() {
     return (
       <React.Fragment>
